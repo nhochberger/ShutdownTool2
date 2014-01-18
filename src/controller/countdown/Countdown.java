fa209bbe8609308b8efc84f84e5f90a61144c197
Nico Hochberger <nico@irrwisch.net>
2014-01-18 20:40:51 +0100

    began work on Countdown

---
+++ src/controller/countdown/Countdown.java
@@ -0,0 +1,26 @@
+package controller.countdown;
+
+import org.joda.time.Period;
+
+public class Countdown {
+
+	private final Period startPeriod;
+
+	public Countdown(Period period) {
+		super();
+		this.startPeriod = period;
+	}
+
+	public void start() {
+
+	}
+
+	public void stop() {
+
+	}
+
+	public void pause() {
+
+	}
+
+}
